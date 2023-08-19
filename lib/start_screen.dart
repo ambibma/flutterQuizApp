42052b42b85def86eb3f36a1d017a590d397c683
ambi hidalgo <ambio.pk@gmail.com>
2023-08-19 16:08:48 -0700

    Add basic start screen

---
+++ lib/start_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(163, 255, 255, 255),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          const Text(
+            'Learn Flutter duh!`',
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            onPressed: () {},
+            label: const Text('Hello'),
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            icon: const Icon(Icons.arrow_right_alt),
+          )
+        ],
+      ),
+    );
+  }
+}
