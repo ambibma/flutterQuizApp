7791203c7b20377e55d34e2970d112a42248faa4
ambi hidalgo <ambio.pk@gmail.com>
2023-08-19 16:15:43 -0700

    Refactor into new component

---
+++ lib/quiz.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.purple, Colors.pinkAccent],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: const StartScreen()),
+      ),
+    );
+  }
+}
