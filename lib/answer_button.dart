e095ad535634b57c75fdeba0e313ec11f4401828
ambi hidalgo <ambio.pk@gmail.com>
2023-08-20 14:52:50 -0700

    Add quiz questions, add dynamically changing answer widgets with state

---
+++ lib/answer_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(this.answerText, this.onTap, {super.key});
+
+  final String answerText;
+  final void Function() onTap;
+
+  @override
+  Widget build(context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 40,
+        ),
+        backgroundColor: const Color.fromARGB(255, 34, 11, 105),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(40),
+        ),
+      ),
+      child: Text(
+        answerText,
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
---
+++ lib/questions_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class QuestionScreen extends StatefulWidget {
+  const QuestionScreen({super.key});
+
+  @override
+  State<QuestionScreen> createState() {
+    return _QuestionsScreenState();
+  }
+}
+
+class _QuestionsScreenState extends State<QuestionScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() => setState(() => currentQuestionIndex++);
+
+  @override
+  Widget build(context) {
+    final currentQuestion = questions[currentQuestionIndex];
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: GoogleFonts.lato(
+                  color: Color.fromARGB(255, 214, 161, 255),
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(answer, answerQuestion);
+            }),
+          ],
+        ),
+      ),
+    );
+  }
+}
